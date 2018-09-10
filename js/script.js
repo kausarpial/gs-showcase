162f837dda4863227b993d67fbf9523c4bda3115
Kausar Ahmed <kausarahmedpial@gmail.com>
2018-09-11 00:42:59 +0600

    Added Smooth Scoll and AOS

--- js/script.js
+++ js/script.js
@@ -1,13 +1,7 @@
 $(document).ready(function () {
     'use strict';
-});
-
-$(window).on('scroll', function () {
-    'use strict';
-    var windscroll = $(window).scrollTop();
-    if (windscroll >= 100) {
-        $('.main-nav').addClass('nav-bg');
-    } else {
-        $('.main-nav').removeClass('nav-bg');
-    }
+    // -----------------------------
+    //  AOS Initialize
+    // -----------------------------
+    AOS.init();
 });
